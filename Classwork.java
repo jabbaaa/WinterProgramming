27a40cce6b7be9cdb79c4cfa256f945f143366aa
drogana <36487951+drogana@users.noreply.github.com>
2018-02-20 23:54:14 -0800

    Add files via upload
    I dont think that Classwork changed at all. Added a mouselistener to win2. Test it with Map1.png in your folder... and fix the file path for it... lol

--- Classwork.java
+++ Classwork.java
@@ -12,20 +12,20 @@ public class Classwork {
         //SoundNode meow = new SoundNode();
         //toySoldiers.main();       
         //mew.window();}
-        Classwork n = new Classwork();
-        try{n.Classwork();}catch (IOException e){}
+        Classwork n = new Classwork(); //custom class... this class!
+        try{n.Classwork();}catch (IOException e){} //had to use a try-catch due to it being in main.
     }
     
     public void Classwork() throws IOException {
 
-        JFrame world = new JFrame("World");
-        win2 background = new win2();
-        world.setResizable(false);
-        world.setLocationRelativeTo(null);
-        world.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        world.setBounds(0, 0, 1024, 768);
-        world.add(background);
-        world.setVisible(true);
+        JFrame world = new JFrame("World"); //New Java frame named world
+        win2 background = new win2(); //(Extends JPanel which makes it viable to go into JFrame.add();
+        world.setResizable(false); //No RESIZES!
+        world.setLocationRelativeTo(null); //Dont recall now...
+        world.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Never forget this.. soo good.
+        world.setBounds(0, 0, 1024, 768); //top left position + width, height
+        world.add(background);//add the Jpanel object to the JFrame.
+        world.setVisible(true); //show the JFrame
     }
     
 }
--- win2.java
+++ win2.java
@@ -1,12 +1,14 @@
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
+
 /**
  *
  * @author Dragon
@@ -14,20 +16,53 @@ import javax.swing.JPanel;
 public class win2 extends JPanel {
     
     private BufferedImage img;
-    private Graphics2D g2;
+    private Listener theListener;
+    private int x, y;
+    //private Graphics2D g2;
     
     public static final int SCALE = 8;
     
         public void paintComponent(Graphics g){
         g.drawImage(img, 0, 0, 1024, 768, null);
         g.setColor(Color.BLUE);
-        g.fillRect(450, 715, 50, 50);
+        g.fillRect(x, y, 50, 50);
         }
         
-        public win2() throws IOException {
+    private class Listener implements MouseListener {
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+            x = e.getX();
+            y = e.getY();
+            System.out.println("x,y pos:" + x + "," + y);
+            repaint();
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+       }
+        
+    }
+        
+    public win2() throws IOException {
 File f = new File ("C:\\Users\\Dragon\\Documents\\NetBeansProjects\\Classwork\\src\\Map1.png");
 img = new BufferedImage(1024,768, BufferedImage.TYPE_INT_ARGB);
 img = ImageIO.read(f);
-g2 = (Graphics2D) img.getGraphics();
+//this.addMouseListener(theListener);
+//g2 = (Graphics2D) img.getGraphics();
+theListener = new Listener();
+this.addMouseListener(theListener);
         }
 }
